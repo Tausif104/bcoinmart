091eb25d6a3d7f598fd360731786fc480c162807
tausif104 <tausifahmed49@gmail.com>
2024-06-12 04:37:54 +0600

    css fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function () {
     infinite: false,
     speed: 300,
     arrows: true,
+    autoplay: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     prevArrow: $('.prev'),
@@ -34,5 +35,40 @@ jQuery(document).ready(function () {
       // settings: "unslick"
       // instead of a settings object
     ],
+  })
+
+  var ctx = document.getElementById('myDonutChart').getContext('2d')
+  var myDonutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Token Pre-Sale', 'Round-1', 'Round-2', 'Round-3', 'Round-4'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: [150, 50, 100, 75, 150],
+          backgroundColor: [
+            '#7D17FF', // Custom Color 1
+            '#708C51', // Custom Color 2
+            '#D81588', // Custom Color 3
+            '#FBA45A', // Custom Color 4
+            '#D14EFF', // Custom Color 5
+          ],
+
+          borderWidth: 0,
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Donut Chart with Custom Colors',
+        },
+      },
+    },
   })
 })
